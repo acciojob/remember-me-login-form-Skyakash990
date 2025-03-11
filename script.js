c82bab938356a9122676cd21f3f67f9fe439b996
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-03-11 19:06:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+document.getElementById("submit").addEventListener("click",(e)=>{
+	e.preventDefault();
+
+	let username=document.getElementById("username").value;
+	let password=document.getElementById("password").value;
+	let remember=document.getElementById("checkbox").checked;
+
+	alert(`Logged in as ${username}`);
+	if(remember){
+		localStorage.setItem("username",username);
+		localStorage.setItem("password",password);
+	}
+	localstorage.removeItem("username");
+	localstorage.removeItem("password");
+	
+	
+})
+let existing=document.getElementById("existing");
+	existing.addEventListener("click",()=>{
+		let uname=localStorage.getItem("username");
+		alert(`Logged in as ${uname}`);
+	})
